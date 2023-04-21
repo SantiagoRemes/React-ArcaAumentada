e4a48e351949c80a93dba62a01a7af8f513efdec
SantiagoRemes <A01635504@tec.mx>
2023-04-21 01:58:21 -0600

    AAAA

--- src/Pages/components/tabla.js
+++ src/Pages/components/tabla.js
@@ -4,51 +4,28 @@ import Checklist from './checklist';
 
 const Table = (props) =>{
     const type = props.type;
-    const id = props.id;
-    const [RefriporTienda, setRefriporTienda] = useState('')
+    const item = props.item;
 
-    useEffect(() => {
-        fetch(`http://localhost:2000/refrisolicitado/refriportienda/${id}`)
-        .then( (res) => res.json())
-        .then((data)=> setRefriporTienda(data));
-    }, []);
+
 
     if(type === 'curr'){
         return(
-            <div>
-                <table class='table'>
-                    <thead class='tableheader'> 
-                        <tr>
-                            <th>Cantidad</th>
-                            <th>Modelo</th>
-                            <th>Código único EDF</th>
-                            <th># de puertas</th>
-                            <th> Movimiento</th>
-                            <th>Razón</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {RefriporTienda && RefriporTienda.map((item) => (
-                            <tr>
-                                <td>1</td>
-                                <td>{item.idModelo}</td>
-                                <td>{item.idRefrigeradorSolicitado}</td>
-                                <td>{item.puertas}</td>
-                                <td>
-                                    <select>
-                                        <option value='1'>Mantener</option>
-                                        <option value='2'>Cambiar</option>
-                                        <option value='3'>Remover</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    <input type='text' placeholder='Razón'/>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                    <tr>
+                        <td>1</td>
+                        <td>{item.idModelo[0]}</td>
+                        <td>{item.idRefrigeradorSolicitado}</td>
+                        <td>{item.puertas}</td>
+                        <td>
+                            <select>
+                                <option value='1'>Mantener</option>
+                                <option value='2'>Cambiar</option>
+                                <option value='3'>Remover</option>
+                            </select>
+                        </td>
+                        <td>
+                            <input type='text' placeholder='Razón'/>
+                        </td>
+                    </tr>
         )
     }else if(type === 'solic'){
         return(
